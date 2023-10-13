e445fc7cd1d06a6509d3390f4496c4936384c1d5
DoDo <119377201+andy-wren@users.noreply.github.com>
2023-10-13 14:06:03 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PickImage from './screen/camera';
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return <PickImage />;
+  }
+}
